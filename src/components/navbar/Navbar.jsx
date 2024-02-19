08f4083e21decf9c69b02daa40847fb1136c6079
Sanjana <sanjudumy@gmail.com>
2024-02-19 10:53:02 +0530

    Adding multiple folders to the repository

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,52 @@
+
+import { useState } from "react";
+import "./navbar.scss";
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import SearchIcon from '@mui/icons-material/Search';
+
+const Navbar = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  window.onscroll = () => {
+    setIsScrolled(window.pageYOffset === 0 ? false : true);
+    return () => (window.onscroll = null);
+  };
+  return (
+    <div className={isScrolled ? "navbar scrolled" : "navbar"}>
+      <div className="container">
+        <div className="left">
+            <div style={{color:"darkblue"}}>SHOWTIME</div>
+          <img
+            src=""
+            alt=""
+          />
+          <span>Homepage</span>
+          <span>Series</span>
+          <span>Movies</span>
+          <span>New and Popular</span>
+          <span>My List</span>
+          <span>Community</span>
+        </div>
+        <div className="right">
+          <SearchIcon className="icon" />
+          <span>KID</span>
+          <NotificationsIcon className="icon" />
+          <img
+            src="https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+            alt=""
+          />
+          <div className="profile">
+            <ArrowDropDownIcon className="icon" />
+            <div className="options">
+              <span>Settings</span>
+              <span>Logout</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
